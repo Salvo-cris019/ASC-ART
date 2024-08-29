3b6bf1a9816188a855006dbe56e082a3d7af8e38
Salvo-cris019 <bebesalvo06@gmail.com>
2024-08-29 18:15:52 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,16 @@
+greeting = ' _____  ____  _      __ __   ___ '
+greeting1=' / ___/ /    || |    |  |  | /   \ '
+greeting2='(   \_ |  o  || |    |  |  ||     |'
+greeting3=' \__  ||     || |___ |  |  ||  O  |'
+greeting4=' /  \ ||  _  ||     ||  :  ||     |'
+greeting5=' \    ||  |  ||     | \   / |     |'
+greeting6='  \___||__|__||_____|  \_/   \___/ '
+
+print (greeting)
+print (greeting1)
+print (greeting2)
+print (greeting3)
+print (greeting4)
+print (greeting5)
+print (greeting6)
+     
